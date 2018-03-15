179a0c1c044b6154f7447d55795b1c0ea6f2d58f
Hassan Mirzaee <hassan_mirzaee@epam.com>
2018-03-15 14:41:34 +0100

    Added UpdateLambdaMojo.java

---
+++ src/main/java/com/epam/plugin/exceptions/BadConfigurationException.java
@@ -0,0 +1,9 @@
+package com.epam.plugin.exceptions;
+
+public class BadConfigurationException extends Exception {
+
+    public BadConfigurationException(String message){
+        super(message);
+    }
+
+}
