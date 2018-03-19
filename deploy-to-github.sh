2b3179c8a17ec04abfe46e987bd9d9e9641fd5ac
Hassan Mirzaee <hassan_mirzaee@epam.com>
2018-03-19 08:31:32 +0100

    updated deploy-to-github.sh

--- deploy-to-github.sh
+++ deploy-to-github.sh
@@ -3,24 +3,20 @@ version=1.0.0-SNAPSHOT
 echo "Building plugin ..."
 mvn clean install
 
-rm repository_temp -Rf && mkdir repository_temp
+rm repository -Rf && mkdir repository
 
 echo "Installing build into local repository ..."
-mvn install:install-file -DgroupId=com.epam -DartifactId=aws-maven-plugin -Dversion=$version -Dfile=target/aws-maven-plugin-$version.jar -Dpackaging=jar -DgeneratePom=false -DpomFile=pom.xml -DlocalRepositoryPath=repository_temp -DcreateChecksum=true
+mvn install:install-file -DgroupId=com.epam -DartifactId=aws-maven-plugin -Dversion=$version -Dfile=target/aws-maven-plugin-$version.jar -Dpackaging=jar -DgeneratePom=false -DpomFile=pom.xml -DlocalRepositoryPath=repository -DcreateChecksum=true
 
 echo "Checkout mvn-repo branch"
 git checkout mvn-repo
-
-echo "Preparing repository ..."
-cp repository_temp/* repository -rf
-rm repository_temp -Rf
 
 
 echo "Committing changes on mvn-repo ..."
 git add repository && git commit -m "release $version"
 
 echo "Publishing to github ..."
-git push origin mvn-repo
+git push origin mvn-repo -f
 echo "Checkout to master"
 git checkout master
 
